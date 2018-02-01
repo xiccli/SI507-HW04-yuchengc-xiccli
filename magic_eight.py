f507dadfec6faaf86195caaa9cebb533d3c776b5
xiccli <xiccli@umich.edu>
2018-02-01 15:00:41 -0500

    check

--- magic_eight.py
+++ magic_eight.py
@@ -2,3 +2,16 @@
 def ask_answer():
     return input("What is your question?")
 a1=ask_answer()#python file for HW4
+
+# check if user input is a question
+while a1 != "":
+    if "?" in a1:
+        print (choice(ans_list))
+        a1=ask_answer()
+    else:
+        print ("I’m sorry, I can only answer questions.")
+        st = input("If you want to stop, please input 'quit'; else, input your question:")
+        if st !="quit":
+            a1=ask_answer()
+        else:
+            break
